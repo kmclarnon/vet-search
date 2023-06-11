fba824ad54dbe7a19eeb546dbc636169f1f68497
Kevin McLarnon <kevin@quasar.tk>
2023-06-11 13:05:18 -0400

    fix import again

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import os
 import sys
 import glob
-import pyperclip
+
+# import required modules below
 
 parent_folder_path = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(parent_folder_path)
 sys.path.append(os.path.join(parent_folder_path, "lib"))
 sys.path.append(os.path.join(parent_folder_path, "plugin"))
 
-# this has to come after we modify the system path
+# these have to come after we modify the system path
 from flowlauncher import FlowLauncher
+import pyperclip
 
 SEARCH_PATH = "FILL-ME-IN"
 
