079b34c0b9e62bab4446d715e2b49e22febe3716
Kevin McLarnon <kevin@quasar.tk>
2023-06-11 12:37:15 -0400

    fix import (hopefully)

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
 import sys
-from flowlauncher import FlowLauncher
 import webbrowser
 
 parent_folder_path = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(parent_folder_path)
 sys.path.append(os.path.join(parent_folder_path, "lib"))
 sys.path.append(os.path.join(parent_folder_path, "plugin"))
+
+# this has to come after we modify the system path
+from flowlauncher import FlowLauncher
 
 
 class HelloWorld(FlowLauncher):
